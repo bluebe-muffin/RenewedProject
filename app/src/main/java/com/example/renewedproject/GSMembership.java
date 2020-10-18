bf1bfcfb379112c9d4a13b06c72e6c7941b9b220
김소연 <thdus7739@gmail.com>
2020-10-18 19:31:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/renewedproject/GSMembership.java
@@ -0,0 +1,19 @@
+package com.example.renewedproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class GSMembership extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gsmembership_layout,container,false);
+    }
+}
